5a39cadbb28c4686484b2f566015e0b89f60d6bc
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2026-03-18 12:32:40 +0100

    add limit and offset to paginate get all data from db. Also add PaginateDto and force nest to transform query data.

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.setGlobalPrefix('api/v2');
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     })
   );
 
