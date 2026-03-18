cdbbfdbdf1dd5bd1b07a91bc05a15965432fe2e7
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2026-03-18 19:47:32 +0100

    remove unnecessary constructor with console.log

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,8 +20,4 @@ import { SeedModule } from './seed/seed.module';
     SeedModule,
   ],
 })
-export class AppModule {
-  constructor() {
-    console.log(process.env);
-  }
-}
+export class AppModule {}
