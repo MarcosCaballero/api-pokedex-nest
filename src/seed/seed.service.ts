e5ff7b44cb9328d5168f3d771b4d766b6aa52459
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2026-03-18 10:51:04 +0100

    add poke response interface and use axios to get data on seed service.

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-reponse.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    for (const pokemon of data.results) {
+      const segments: string[] = pokemon.url.split('/');
+
+      const rawNo: string | undefined = segments.at(-2);
+
+      if (!rawNo) {
+        continue;
+      }
+
+      const parsedNo: number = Number.parseInt(rawNo);
+
+      console.log({ name: pokemon.name, no: parsedNo });
+    }
+
+    return data.results;
   }
 }
